25477fa80424fb8527254ef0b1cec4e0aead94f4
Joram Wambugu <artisan254@gmail.com>
2021-05-20 14:38:25 +0300

    Slices: Complete the slices lesson

--- pkg/arrays_slices/sum.go
+++ pkg/arrays_slices/sum.go
@@ -10,3 +10,31 @@ func Sum(numbers []int) int {
 
 	return sum
 }
+
+// SumAll takes a varying number of slices, returns a new slice containing the totals for each slice passed in
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+//SumAllTails calculates the totals of the "tails" of each slice.
+// The tail of a collection is all the items apart from the first one (the "head")
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		if len(numbers) != 0 {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		} else {
+			sums = append(sums, 0)
+		}
+	}
+
+	return sums
+}
